50da789230dbafe8505692af7aaae4bff6a5bf77
Archana <arc243archana@gmail.com>
2025-04-02 16:00:12 +0530

    done

--- src/Calc/Calculator.java
+++ src/Calc/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator implements ActionListener{
     JFrame frame;
     JTextField textfield;
 
-    JButton[] numButtom=new JButton[10];
+    JButton[] numButton=new JButton[10];
     JButton[] fnButton=new JButton[8];
 
     JButton add, sub,multi,div,dec,eq,del,clr;
@@ -27,7 +27,69 @@ public class Calculator implements ActionListener{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,550);
         frame.setLayout(null);
+
+
+        textfield=new JTextField();
+        textfield.setBounds(30,25,350,55);
+        textfield.setFont(myFont);
+        textfield.setEditable(false);
+
+        add=new JButton("+");
+        sub =new JButton("-");
+        multi=new JButton("*");
+        div=new JButton("/");
+        dec=new JButton(".");
+        eq=new JButton("=");
+        del=new JButton("delete");
+        clr=new JButton("Clear");
+
+        fnButton[0]=add;
+        fnButton[1]=sub;
+        fnButton[2]=multi;
+        fnButton[3]=div;
+        fnButton[4]=dec;
+        fnButton[5]=eq;
+        fnButton[6]=del;
+        fnButton[7]=clr;
+
+        for(int i=0;i<fnButton.length;i++){
+            fnButton[i].addActionListener(this);
+            fnButton[i].setFocusable(false);
+            fnButton[i].setFont(myFont);
+        }
+        for(int i=0;i<numButton.length;i++){
+            numButton[i]= new JButton(String.valueOf(i));
+
+            numButton[i].addActionListener(this);
+            numButton[i].setFocusable(false);
+            numButton[i].setFont(myFont);
+        }
+        del.setBounds(50,430,145,50);
+        clr.setBounds(205,430,145,50);
+
+        panel=new JPanel();
+        panel.setBounds(50,100,300,300);
+        panel.setLayout(new GridLayout(4,4,10,10));
+        panel.setBackground(Color.darkGray);
+
+
+
+        for(int i=0;i<numButton.length;i++){
+            panel.add(numButton[i]);
+        }
+
+        panel.add(dec);
+        panel.add(eq);
+        panel.add(add);
+        panel.add(sub);
+        panel.add(multi);
+        panel.add(div);
+
+        frame.add(panel);
+        frame.add(del);
+        frame.add(clr);
         frame.setVisible(true);
+        frame.add(textfield);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
